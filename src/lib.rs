6390f501a8058b9ac517ceb80b17999f042fcd68
quelledanielle <quelledanielle@users.noreply.github.com>
2018-05-08 16:59:29 -0400

    Add arbitrary thresholds lol

--- src/lib.rs
+++ src/lib.rs
@@ -17,15 +17,20 @@ pub extern "C" fn alloc(size: usize) -> *mut c_void {
 #[no_mangle]
 pub extern "C" fn transform_grayscale(pointer: *mut u8, max_width: usize, max_height: usize) {
     let byte_size = max_width * max_height * VALS_PER_PIXEL;
-    let sl = unsafe { slice::from_raw_parts_mut(pointer, byte_size) };
+    let bytes = unsafe { slice::from_raw_parts_mut(pointer, byte_size) };
 
     for i in (0..byte_size).step_by(VALS_PER_PIXEL) {
-        let vals = &sl[i..i+2].to_vec();
-        let val = avg(&vals);
-        sl[i] = val;
-        sl[i + 1] = val;
-        sl[i + 2] = val;
-        sl[i + 3] = 255;
+        let val = match bytes[i] {
+            0 ..= 40 => 0,
+            40 ..= 85 => 85,
+            85 ..= 170 => 170,
+            _ => 255
+        };
+
+        bytes[i] = val;
+        bytes[i + 1] = val;
+        bytes[i + 2] = val;
+        bytes[i + 3] = 255;
     }
 }
 
